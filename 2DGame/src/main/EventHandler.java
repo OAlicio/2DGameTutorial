2d71a333eeb7f00c3795814993219943ea6921c6
octavioalicio <163463841+OAlicio@users.noreply.github.com>
2024-12-11 08:59:01 +0100

    [EVENT PER LOADING, AND ONE TOUCH EVENT]

--- 2DGame/src/main/EventHandler.java
+++ 2DGame/src/main/EventHandler.java
@@ -2,67 +2,116 @@ package main;
 
 import java.awt.Rectangle;
 
+//CUIDA DOS EVENTOS
 public class EventHandler {
     
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    EventRect eventRect[][];
+    
+    int previousEventX, previousEventY;
+    boolean canTouchEvent = true;
+    
     
     public EventHandler(GamePanel gp) {
         
         this.gp = gp;
         
-        eventRect = new Rectangle();
-        eventRect.x = 23; //QUEREMOSQUEO EVENTO OCORRA MAIS PRA DENTRO DE UM TILE
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
         
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        int col = 0;
+        int row = 0;
+        while(col < gp.maxWorldCol && row < gp.maxWorldRow) {
+           
+        eventRect[col][row] = new EventRect();
+        eventRect[col][row].x = 23; //QUEREMOS QUE O EVENTO OCORRA MAIS PRA DENTRO DE UM TILE
+        eventRect[col][row].y = 23;
+        eventRect[col][row].width = 2;
+        eventRect[col][row].height = 2;
+        
+        eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+        eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+        
+        col++;
+        if(col == gp.maxWorldCol) {
+            col = 0;
+            row++;
+        }
+        }
     }
     
     public void checkEvent() {
         
-        if(hit(27, 16, "right") == true) {
+        // VERIFICA SE O JOGADOR ESTA UM TILE DE DISTANCIA DO ULTIMO EVENTO, SO ACONTECE SE O PLAYER SE MOVER UM TILE E VOLTAR
+        
+        int xDistance = Math.abs(gp.player.worldX - previousEventX);
+        int yDistance = Math.abs(gp.player.worldY - previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+        
+        if(distance > gp.tileSize) {
+            canTouchEvent = true;
+        }
+        // ----------------------------------------------------------------- //
+        
+        if(canTouchEvent == true) {
+            
+          if(hit(27, 16, "right") == true) {
             //EVENTO ACONTECE
-            damagePit(gp.dialogueState);
+            damagePit(27, 16, gp.dialogueState);
+        }
+          
+          if(hit(23, 19, "any") == true) {
+            //EVENTO ACONTECE
+            damagePit(27, 16, gp.dialogueState);
         }
         
         if(hit(23, 12, "up") == true) {
             //EVENTO ACONTECE
-            healingPool(gp.dialogueState);
+            healingPool(23, 12, gp.dialogueState);
         }
         
 //        if(hit(27, 16, "right") == true) {
 //            //EVENTO ACONTECE
 //            teleport(gp.dialogueState);
 //        }
+        }
     }
     
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
         
         boolean hit = false;
         
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
         
-        if(gp.player.solidArea.intersects(eventRect)) {
+        
+         if(gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
             
             if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 
                 hit = true;
+                
+                previousEventX = gp.player.worldX;
+                previousEventY = gp.player.worldY;
             }
         }
+        
+        //DAMAGE PIT OU HEALING UMA VEZ APENAS
+//        if(gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
+//            
+//            if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
+//                
+//                hit = true;
+//            }
+//        }
         
         //REDEFININDO PROS VALORES PADROES
         
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
         
         return hit;
     }
@@ -75,20 +124,32 @@ public class EventHandler {
         gp.player.worldY = gp.tileSize * 10;
     }
     
-    public void damagePit(int gameState) {
+    public void damagePit(int col, int row, int gameState) {
         
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You fall into a pit!";
         gp.player.life -= 1;
+        //eventRect[col][row].eventDone = true;
+        canTouchEvent = false;
     }
     
-    public void healingPool(int gameState) {
+    //DAMAGE PIT CONFIGURADO PRA DAR APENAS UM HIT POR LOAD
+//    public void damagePit(int col, int row, int gameState) {
+//        
+//        gp.gameState = gameState;
+//        gp.ui.currentDialogue = "You fall into a pit!";
+//        gp.player.life -= 1;
+//        eventRect[col][row].eventDone = true;
+//    }
+    
+    public void healingPool(int col, int row, int gameState) {
         
         if(gp.keyH.enterPressed == true) {
             
             gp.gameState = gameState;
             gp.ui.currentDialogue = "You drink the water.\nYour life has been recovered";
             gp.player.life = gp.player.maxLife;
+            canTouchEvent = false;
         }
     }
 }
---
+++ 2DGame/src/main/EventRect.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.awt.Rectangle;
+
+//CUIDA DO RECTANGULO PROS EVENTOS
+public class EventRect extends Rectangle {
+    
+    int eventRectDefaultX, eventRectDefaultY;
+    boolean eventDone = false;      
+}
