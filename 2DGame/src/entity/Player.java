a81ff288019ecf8fa85f03d5b27e2465d29e1c53
octavioalicio <163463841+OAlicio@users.noreply.github.com>
2025-01-26 21:18:59 +0100

    Game Over Title improvements
    Smashing enter key cannot retry

--- 2DGame/src/entity/Player.java
+++ 2DGame/src/entity/Player.java
@@ -306,6 +306,7 @@ public class Player extends Entity {
         
         if(life <= 0) {
             gp.gameState = gp.gameOverState;
+            gp.ui.commandNum = -1;
             gp.stopMusic();
             gp.playSE(12);
         }
